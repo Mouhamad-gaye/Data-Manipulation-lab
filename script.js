203090a89251ae6eef57daa5f6e773bffe76c7e0
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-03-31 05:22:09 -0600

    Third commit

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ subMenuEl.classList.add = 'flex-around'
 
 subMenuEl.style.top = '0'
 
-// Part 4
+// Part 4 & Part 5
 
 
 
@@ -67,25 +67,45 @@ function handleClick(e) {
   // console.log('A')
   topMenuLinks.forEach(link => link.classList.remove('active')); 
   e.target.classList.toggle('active'); 
+  const linkObj = menuLinks.find(link => link.text === event.target.textContent);
+
+
+  if (linkObj && linkObj.subLinks) {
+      subMenuEl.style.top = '100%'; 
+      buildSubMenu(linkObj.subLinks); 
+  } else {
+      subMenuEl.style.top = '0'; 
+  }
 }
 
-menuLinks.forEach((link) => {
+subMenuEl.addEventListener('click', handleClick)
+
+function handleClick(e) {
+  e.preventDefault();
+  if(e.srcElement.nodeName !== 'A') {
+    console.log('not A')
+    return
+  }
+console.log('A')
+topMenuLinks.forEach(link => link.classList.remove('active'))
+mainEl.innerHTML = `<h1>About</h1>`;
+}
+
+//Helper Function
+
+function buildSubMenu(subLinks){
+  subMenuEl.innerHTML = 'About'
+ subLinks.forEach(link => {
+  let subLinkEl= document.createElement('a')
+  subLinkEl.setAttribute('href', link.href)
+  subLinkEl.textContent = link.text
+  subMenuEl.appendChild(subLinkEl)
+ })
+}
+
+menuLinks.forEach(link => {
   let newAnc = document.createElement('a')
   newAnc.setAttribute('href', link.href)
   newAnc.textContent = link.text;
   topMenuEl.appendChild(newAnc)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
